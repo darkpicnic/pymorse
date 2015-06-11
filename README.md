8069890d14c3d1443f851923e9d19a56f03a2d20
Sebastian Lemery <sebastian@lemery.io>
2015-06-11 11:35:11 -0700

    setup and readme updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pymorse
-A library for easily generating morse code for training or transmission
+A library for easily generating morse code for training or transmission. Currently in development.
+
+
+## Features (Coming soon)
+
+* Generate morse code from a string
+* Write generated morse audio to file
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='pymorse',
+    version='0.1',
+    description='A library for easily generating morse code for training or transmission',
+    author='Sebastian Lemery',
+    author_email='sebastian@lemery.io',
+    url='https://github.com/darkpicnic/pymorse',
+    packages=find_packages(),
+    install_requires=[
+
+    ]
+)
+
